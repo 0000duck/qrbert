e86ac0eb67f4237d2d777ec1959f97478ae9ed25
JESaucedo <60938062+JESaucedo@users.noreply.github.com>
2022-02-12 17:24:41 -0800

    Added functionality to a button

--- QRbertSLN/QRbert/MainWindow.xaml.cs
+++ QRbertSLN/QRbert/MainWindow.xaml.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -12,17 +13,47 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Image = System.Windows.Controls.Image;
+using Size = System.Windows.Size;
+using ZBar;
+
 
 namespace qrbertApp
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    ///
+    
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
         }
+        private Bitmap MyImage ;
+
+
+        private void StartCamera(object sender, RoutedEventArgs e)
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = "D:/ZBar/bin/zbarcam";
+            process.Start();
+            
+        }
+        /*public void ShowMyImage(String fileToDisplay, int xSize, int ySize)
+        {
+            // Sets up an image object to be displayed.
+            if (MyImage != null)
+            {
+                MyImage.Dispose();
+            }
+
+            // Stretches the image to fit the pictureBox.
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage ;
+            MyImage = new Bitmap(fileToDisplay);
+            pictureBox1.ClientSize = new Size(xSize, ySize);
+            pictureBox1.Image = (Image) MyImage ;
+        }*/
     }
 }
