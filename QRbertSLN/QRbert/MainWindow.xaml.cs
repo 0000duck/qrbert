03db016798a4ce0a50d1d15ef9d8d3d95bea3fec
JESaucedo <60938062+JESaucedo@users.noreply.github.com>
2022-03-02 19:00:27 -0800

    Changed Namespace
    Changed qrbertapp to QRbert

--- QRbertSLN/QRbert/MainWindow.xaml.cs
+++ QRbertSLN/QRbert/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+
+using System.Diagnostics;
+using System.IO;
+
 using System.Windows;
 using System.Windows.Threading;
 
@@ -28,6 +32,21 @@ namespace QRbert
             timer.Tick -= TimerTick;
             // WelcomeScreen.Close();
         }
-        
+
+
+        private void StartCamera(object sender, RoutedEventArgs e)
+        {
+            //Process process = new Process();
+            //String pathfile = Path.GetFullPath("zbarcam");
+            //Console.WriteLine(pathfile);
+            //process.StartInfo.FileName = pathfile;
+            //process.Start();
+            
+            
+            //System.Diagnostics.Process.Start("zbarcam.exe");
+            //Page generateQRCodePage = new GenerateQrCode();
+            //this.Content = generateQRCodePage;
+        }
+
     }
 }
