82f1bdbd556915c9e6a5bd2350e7ca8a3b902fbf
DpTwentyTwo <47573804+DpTwentyTwo@users.noreply.github.com>
2022-03-04 09:17:16 -0800

    removed registration date

--- QRbertSLN/QRbert/Register.xaml.cs
+++ QRbertSLN/QRbert/Register.xaml.cs
@@ -4,12 +4,16 @@ using System.Windows.Controls;
 using System.Windows;
 using System.Data.SqlClient;
 
+
+//using SqlCommand = Microsoft.Data.SqlClient.SqlCommand;
+
+
 namespace QRbert;
 
 public partial class Register : Page
 {
     // connects DB to Register Page
-  string connectionString = @"Data Source = qrbert-rds1; 
+  String connectionString = @"Data Source = qrbert-rds1.cfe8s1xr87h2.us-west-1.rds.amazonaws.com; 
                                 Initial Catalog = QRbertDB; User ID = rds1_admin; Password = rds1_admin;";
 
 
@@ -21,35 +25,34 @@ public partial class Register : Page
     // Link the "sign Up" button to DB to save user registration info
     private void RegUser_Button(object sender, EventArgs e)
     {
-        /* if (txtEmail.Text == "" || txtPassword.Text == "")
+        if (txtEmail.Text == "" || txtPassword.Text == "")
             MessageBox.Show("Please fill out all mandatory fields");
         else if (txtPassword.Text != txtConfirmPassword.Text)
             MessageBox.Show("Passwords Do Not Match");
         else
         {
-*/
-
-        using SqlConnection sqlCon = new SqlConnection(connectionString);
-        sqlCon.Open();
-        SqlCommand sqlCmd = new SqlCommand("RegUser", sqlCon);
-        sqlCmd.CommandType = CommandType.StoredProcedure;
-        sqlCmd.Parameters.AddWithValue("@Email", txtEmail.Text.Trim());
-        sqlCmd.Parameters.AddWithValue("@Password", txtPassword.Text.Trim());
-        sqlCmd.Parameters.AddWithValue("@Faculty_Role", txtFacultyRole.Text.Trim());
-        sqlCmd.Parameters.AddWithValue("@FirstName", txtFirstName.Text.Trim());
-        sqlCmd.Parameters.AddWithValue("@LastName", txtLastName.Text.Trim());
 
 
-        sqlCmd.ExecuteNonQuery();
-        MessageBox.Show("Sign Up Complete ");
-        Clear();
+            using SqlConnection sqlCon = new SqlConnection(connectionString);
+
+            sqlCon.Open();
+
+            SqlCommand sqlCmd = new SqlCommand("RegUser", sqlCon);
+
+            sqlCmd.CommandType = CommandType.StoredProcedure;
+            sqlCmd.Parameters.AddWithValue("@Email", txtEmail.Text);
+            sqlCmd.Parameters.AddWithValue("@Password", txtPassword.Text);
+            sqlCmd.Parameters.AddWithValue("@Faculty_Role", txtFacultyRole.Text);
+            sqlCmd.Parameters.AddWithValue("@FirstName", txtFirstName.Text);
+            sqlCmd.Parameters.AddWithValue("@LastName", txtLastName.Text);
+
+            sqlCmd.ExecuteNonQuery();
+            MessageBox.Show("Sign Up Complete ");
+
+        }
+
     }
 
-    void Clear()
-    {
-        txtFacultyRole.Text = txtFirstName.Text = txtLastName.Text = txtEmail.Text = txtPassword.Text = txtConfirmPassword.Text = "";
-    }
-    
 }
 
 
