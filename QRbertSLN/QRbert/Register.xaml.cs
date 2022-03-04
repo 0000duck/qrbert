4d8936fac3437a801ccf69ac5660f87f1044cc2d
DpTwentyTwo <47573804+DpTwentyTwo@users.noreply.github.com>
2022-03-04 09:37:53 -0800

    clean up code

--- QRbertSLN/QRbert/Register.xaml.cs
+++ QRbertSLN/QRbert/Register.xaml.cs
@@ -12,7 +12,13 @@ namespace QRbert;
 
 public partial class Register : Page
 {
-    // connects DB to Register Page
+    /*
+     * connects DB to Register Page
+     * Data source is the name of the DB server
+     * Initial Catalog the QRbert database we want to connect to
+     * User name and Password -> temp log in solution until we find a more secure way to log into the DB
+     * so that the log in credentials aren't in the code for all to see
+     */ 
   String connectionString = @"Data Source = qrbert-rds1.cfe8s1xr87h2.us-west-1.rds.amazonaws.com; 
                                 Initial Catalog = QRbertDB; User ID = rds1_admin; Password = rds1_admin;";
 
@@ -25,18 +31,15 @@ public partial class Register : Page
     // Link the "sign Up" button to DB to save user registration info
     private void RegUser_Button(object sender, EventArgs e)
     {
+        // make sure mandatory fields are typed in
         if (txtEmail.Text == "" || txtPassword.Text == "")
             MessageBox.Show("Please fill out all mandatory fields");
         else if (txtPassword.Text != txtConfirmPassword.Text)
             MessageBox.Show("Passwords Do Not Match");
         else
         {
-
-
             using SqlConnection sqlCon = new SqlConnection(connectionString);
-
             sqlCon.Open();
-
             SqlCommand sqlCmd = new SqlCommand("RegUser", sqlCon);
 
             sqlCmd.CommandType = CommandType.StoredProcedure;
@@ -48,11 +51,8 @@ public partial class Register : Page
 
             sqlCmd.ExecuteNonQuery();
             MessageBox.Show("Sign Up Complete ");
-
         }
-
     }
-
 }
 
 
