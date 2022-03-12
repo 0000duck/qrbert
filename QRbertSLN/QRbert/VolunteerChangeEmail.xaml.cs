1c754c69a4e09b034d20e7b6d8d83a66a92604c7
marisoltej27 <marisol.tejadaramirez@student.csulb.edu>
2022-03-11 21:52:37 -0800

    This is the change email page for volunteer users

---
+++ QRbertSLN/QRbert/VolunteerChangeEmail.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace QRbert;
+
+public partial class VolunteerChangeEmail : Page
+{
+    public VolunteerChangeEmail()
+    {
+        InitializeComponent();
+    }
+}
