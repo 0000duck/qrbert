5861961f82beb0ea544d7a1c02b6a77701fcf269
JESaucedo <60938062+JESaucedo@users.noreply.github.com>
2022-03-31 12:05:48 -0700

    Documentation on GetStarted.xaml, GetStarted.xaml.cs, Register.xaml
    Added comments to all these files.

--- QRbertSLN/QRbert/GetStarted.xaml.cs
+++ QRbertSLN/QRbert/GetStarted.xaml.cs
@@ -11,7 +11,11 @@ public partial class GetStarted : Window
         // this.WindowState = WindowState.Maximized;
         InitializeComponent();
     }
-
+/// <summary>
+/// Function that creates a new LogInWindow window
+/// </summary>
+/// <param name="sender"></param>
+/// <param name="e"></param>
     private void GetStartedBtn_Click(object sender, RoutedEventArgs e)
     {
         Window LogInWindow = new LogIn_Register();
