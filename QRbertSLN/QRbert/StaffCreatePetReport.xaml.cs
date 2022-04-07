8d59556d1bafe36e37424f8c2f936fbb580abe42
marisoltej27 <marisol.tejadaramirez@student.csulb.edu>
2022-04-07 14:21:42 -0700

    Started designing staff create pet report page

---
+++ QRbertSLN/QRbert/StaffCreatePetReport.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows;
+
+namespace QRbert;
+
+public partial class StaffCreatePetReport : Window
+{
+    public StaffCreatePetReport()
+    {
+        InitializeComponent();
+    }
+}
