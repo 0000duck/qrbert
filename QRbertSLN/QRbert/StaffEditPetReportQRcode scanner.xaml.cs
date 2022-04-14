5d0d7ee8cc70ad2bae66553692129358d0cf7bce
marisoltej27 <marisol.tejadaramirez@student.csulb.edu>
2022-04-14 16:37:06 -0700

    StaffEditPetReportQrCodeScanner
    When the staff user clicks on the "Edit" button on the pet report page the user will be redirected to this page where they can scan the qr code of the pet whose pet report they desire to edit

---
+++ QRbertSLN/QRbert/StaffEditPetReportQRcode scanner.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows;
+
+namespace QRbert;
+
+public partial class StaffEditPetReportQRcode_scanner : Window
+{
+    public StaffEditPetReportQRcode_scanner()
+    {
+        InitializeComponent();
+    }
+}
