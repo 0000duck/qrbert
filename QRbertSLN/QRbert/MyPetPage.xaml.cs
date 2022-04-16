6c05737a4e919fb4cc6c5ff49a02dbb64bd42e41
marisoltej27 <marisol.tejadaramirez@student.csulb.edu>
2022-04-15 23:42:38 -0700

    This is the pet page for staff

---
+++ QRbertSLN/QRbert/MyPetPage.xaml.cs
@@ -0,0 +1,24 @@
+using System.Windows;
+
+namespace QRbert;
+
+public partial class SelectAPet : Window
+{
+    public SelectAPet()
+    {
+        InitializeComponent();
+    }
+    private void HomeStaffPortalBtn_Click(object sender, RoutedEventArgs e)
+    {
+        
+    }
+
+    private void StaffMyAccountBtn_Click(object sender, RoutedEventArgs e)
+    {
+        
+    }
+    private void LogOutBtn_Click(object sender, RoutedEventArgs e)
+    {
+        
+    }
+}
