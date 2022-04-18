c6726382d46ec6aa849d16febac7c9d8e4d90df9
Denise <47573804+DpTwentyTwo@users.noreply.github.com>
2022-04-17 20:16:50 -0700

    Reg New pet and display pet name

--- QRbertSLN/QRbert/StaffCreatePetReport.xaml.cs
+++ QRbertSLN/QRbert/StaffCreatePetReport.xaml.cs
@@ -1,9 +1,14 @@
 using System.Windows;
-
+using System.Data;
+using System.Windows;
+using System.Windows.Controls;
+using System.Data.SqlClient;
 namespace QRbert;
 
 public partial class StaffCreatePetReport : Window
 {
+    string connectionString = @"Data Source = qrbert-rds1.cfe8s1xr87h2.us-west-1.rds.amazonaws.com; 
+                                Initial Catalog = QRbertDB; User ID = rds1_admin; Password = rds1_admin;";
     public StaffCreatePetReport()
     {
         InitializeComponent();
@@ -108,4 +113,25 @@ public partial class StaffCreatePetReport : Window
         Switcher.StaffPageSwitch(new StaffRoundingRules());
         this.Close();
     }
+    
+    private void CreatePet_Button(object sender, RoutedEventArgs e)
+    {
+        using SqlConnection sqlCon = new SqlConnection(connectionString);
+        sqlCon.Open();
+            
+        /* User Input stored in Pet table
+         * NewPet is the stored procedure created in the DB that inserts data into each row of the Pet table
+        */ 
+        SqlCommand sqlCmd = new SqlCommand("NewPet", sqlCon);
+        sqlCmd.CommandType = CommandType.StoredProcedure;
+        sqlCmd.Parameters.AddWithValue("@PetName", txtPetName.Text);
+        sqlCmd.Parameters.AddWithValue("@DOB",txtDOB.Text);
+        sqlCmd.Parameters.AddWithValue("@Type", txtType.Text);
+        sqlCmd.Parameters.AddWithValue("@Breed", txtBreed.Text);
+        sqlCmd.Parameters.AddWithValue("@Gender ", txtGender.Text);
+        sqlCmd.ExecuteNonQuery();
+        MessageBox.Show("You have Registered a New Pet");
+    }
+    
+
 }
