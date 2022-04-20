7db53777b6378189128293650710341ace4c9e41
marisoltej27 <marisol.tejadaramirez@student.csulb.edu>
2022-04-20 16:58:01 -0700

    This is the forgot password page for users that are not signed

---
+++ QRbertSLN/QRbert/UserForgotPassword.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows;
+
+namespace QRbert;
+
+public partial class UserForgotPassword : Window
+{
+    public UserForgotPassword()
+    {
+        InitializeComponent();
+    }
+
+    private void HomeStaffPortalBtn_Click(object sender, RoutedEventArgs e)
+    {
+    }
+}
