cdfb03d3abf4925c85a8158b385e91be3f4abe09
JESaucedo <60938062+JESaucedo@users.noreply.github.com>
2022-04-21 14:51:08 -0700

    Update Timesheet Display
    Made it so that the timesheet is displayed

--- QRbertSLN/QRbert/LogIn_Register.xaml.cs
+++ QRbertSLN/QRbert/LogIn_Register.xaml.cs
@@ -1,5 +1,9 @@
 ﻿using System.Windows;
 using System.Data.SqlClient;
+using System.Windows.Controls;
+using Aspose.Pdf;
+using Aspose.Pdf.Text;
+using BitMiracle.Docotic.Pdf;
 using Window = System.Windows.Window;
 using static QRbert.QRCodeScanner;
 
@@ -33,7 +37,7 @@ namespace QRbert
 
             // email and password input from the user
             string emailInput = txtEmail.Text;
-            string pwInput = txtPassword.Text;
+            string pwInput = txtPassword.Password;
 
             // Given the username, Denise will query the database to retrieve the account type
             string msg = Switcher.VerifyRole("SELECT count(*) From QRbertTables.Registration where email = '" + emailInput +
@@ -130,6 +134,116 @@ namespace QRbert
                  Switcher.LogIn_RegisterSwitch(new VolunteerPortal());
                  this.Close();
              }
+        }
+
+
+        //If there is text in the TextBox, the Email text goes away
+        //Returns if there is no text in the TextBox
+        private void TxtEmail_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            txtSignInEmail.Visibility = Visibility.Visible;
+            if (txtEmail.Text.Length > 0)
+            {
+                txtSignInEmail.Visibility = Visibility.Hidden;
+            }
+        }
+
+        //If there is text in the PasswordBox, the Password text goes away
+        //Returns if there is no text in the PasswordBox
+        private void TxtPassword_OnPasswordChanged(object sender, RoutedEventArgs e)
+        {
+            txtSignInPassword.Visibility = Visibility.Visible;
+            if (txtPassword.Password.Length > 0)
+            {
+                txtSignInPassword.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void makeTable(object sender, RoutedEventArgs routedEventArgs)
+        {
+            Document document = new Document();
+
+// Add page
+            Aspose.Pdf.Page page = document.Pages.Add();
+
+// Add text to new page
+            
+            TextFragment textFragment = new TextFragment("Hello World!");
+            textFragment.TextState.FontSize = 120;
+
+            Table table = new Table();
+            
+            table.ColumnAdjustment = ColumnAdjustment.AutoFitToWindow;
+            // Add row to table
+            Aspose.Pdf.Row header = table.Rows.Add();
+            // Add table cells
+            header.Cells.Add("User ID");
+            header.Cells.Add("First Name");
+            header.Cells.Add("Last Name");
+            Row header2 = table.Rows.Add();
+            header2.Cells.Add("      ");
+            Row header3 = table.Rows.Add();
+            header3.Cells.Add("      ");
+            
+
+            Table timeTable = new Table();
+            timeTable.ColumnWidths = "70 2cm";
+            //timeTable.ColumnAdjustment = ColumnAdjustment.AutoFitToWindow;
+            Aspose.Pdf.Row timeRows = timeTable.Rows.Add();
+            var testCell1 = timeRows.Cells.Add("Mon");
+            testCell1.ColSpan = 2;
+            var testCell2 = timeRows.Cells.Add("Tues");
+            testCell2.ColSpan = 2;
+            var testCell3 = timeRows.Cells.Add("Wed");
+            testCell3.ColSpan = 2;
+            var testCell4 = timeRows.Cells.Add("Thurs");
+            testCell4.ColSpan = 2;
+            var testCell5 = timeRows.Cells.Add("Fri");
+            testCell5.ColSpan = 2;
+            
+            
+            for (int row_count = 1; row_count < 2; row_count++)
+            {
+                // Add row to table
+                Aspose.Pdf.Row row = timeTable.Rows.Add();
+                // Add table cells
+                string msg =
+                    Switcher.VerifyRole(
+                        "Select QRbertTables.TimeSheet.Clock_In  FROM ((QRbertTables.TimeSheet INNER JOIN QRbertTables.Volunteer ON QRbertTables.TimeSheet.ID = QRbertTables.Volunteer.ID));");
+                row.Cells.Add(msg);
+                row.Cells.Add("Column (" + row_count + ", 2)");
+                row.Cells.Add("Column (" + row_count + ", 3)");
+                row.Cells.Add("Column (" + row_count + ", 4)");
+                row.Cells.Add("Column (" + row_count + ", 5)");
+                row.Cells.Add("Column (" + row_count + ", 6)");
+                row.Cells.Add("Column (" + row_count + ", 7)");
+                row.Cells.Add("Column (" + row_count + ", 8)");
+                row.Cells.Add("Column (" + row_count + ", 9)");
+                row.Cells.Add("Column (" + row_count + ", 0)");
+            }
+            page.Paragraphs.Add(table);
+            page.Paragraphs.Add(timeTable);
+            page.PageInfo.IsLandscape = true;
+            
+
+// Save PDF 
+            document.Save("document.pdf");
+            PdfDocument pdf = new PdfDocument("document.pdf");
+            PdfDrawOptions options = PdfDrawOptions.CreateZoom(150);
+            options.BackgroundColor = new PdfRgbColor(255, 255, 255); // white background, transparent by default
+            //options.Format = PdfDrawFormat.Jpeg;
+            PdfPage page2 = pdf.Pages[0];
+            PdfBox cropBoxBefore = page2.CropBox;
+
+            //page2.CropBox = new PdfBox(0, cropBoxBefore.Height - 256, 256, cropBoxBefore.Height);
+            pdf.Pages[0].Save("result.jpg",options);
+            
+            
+            
+            Switcher.VolunteerPortalSwitch(new VolunteerViewTimesheets());
+            //Switcher.VolunteerPortalSwitch(new VolunteerViewTimesheets());
+            //this.Close();
+            
         }
     }
 
