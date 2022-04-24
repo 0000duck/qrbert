850b03302f79619c1d43f0bcd8be51747edea359
marisoltej27 <marisol.tejadaramirez@student.csulb.edu>
2022-04-23 22:59:40 -0700

    Added bell icon for notifications in all current staff pages

--- QRbertSLN/QRbert/AddPetTreatment.xaml.cs
+++ QRbertSLN/QRbert/AddPetTreatment.xaml.cs
@@ -10,7 +10,10 @@ public partial class AddPetTreatment : Window
         
         
     }
-    
+    private void NotificationBtn_Click(object sender, RoutedEventArgs e)
+    {
+        
+    }
     /// <summary>
     /// Redirects staff to their MyAccount page via button click
     /// Since the portal and the MyAccount are both pages, they should be easily navigable
